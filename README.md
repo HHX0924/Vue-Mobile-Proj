9a6fc520cddb82f45716e69ebff20cd0d0dd4799
HHX0924 <2697430277@qq.com>
2023-08-14 18:38:16 +0800

    continue

--- README.md
+++ README.md
@@ -1,5 +1,66 @@
 # vue-shopping
-
+```
+1.基于VueCli创建项目
+2.使用vant组件库
+3.postcss插件 实现vw适配
+yarn add postcss-px-to-viewport@1.1.1 -D
+新建postcss的配置文件postcss.config.js
+4.一级路由配置
+5.二级路由配置
+路由出口 <router-view></router-view>
+6.request模块 - axios封装
+实例 请求拦截器 响应拦截器 
+7.完成获取图形验证码
+8.封装api接口-图形验证码
+9.toast 轻提示
+10.短信验证倒计时 定时器
+11.封装api接口 - 登录功能
+12.响应拦截器统一处理错误提示
+13.将登录权证信息存入 vuex
+14.vuex持久化处理
+    localStorage.get/set/remove Item
+15.请求拦截器添加请求 loading 效果 
+16. 登录访问拦截 - 路由前置守卫
+router.beforeEach((to, from, next) => {
+   1. to   往哪里去， 到哪去的路由信息对象  
+   2. from 从哪里来， 从哪来的路由信息对象
+   3. next() 是否放行
+      如果next()调用，就是放行
+      next(路径) 拦截到某个路径页面
+})
+17.首页动态渲染
+    1.封装获取首页信息的api
+    2.created函数里发送请求获取信息
+    3.动态渲染
+    4.父传子中，传的是对象，接收时的默认值defaul是函数
+        props: {
+        item: {
+          type: Object,
+          default: () => {
+            return {}
+          }
+        }
+      }
+18.搜索历史基本渲染
+    1.点击搜索按钮或者地下记录都能搜索
+    ①若之前没有相同的搜索，直接添加到最前面 unshit
+    ②若之前有相同的搜索，先删除再添加到最前面
+        indexOf判断
+        splice删除
+    2.清空历史
+    3.搜索历史持久化
+    4.跳转 传参（搜索内容）
+19.搜索列表
+    1.封装api
+    2.this.$route.query获取参数
+    3.发送请求渲染
+20.商品分类页
+    封装api发送请求获取数据
+21.商品详情页
+    封装api发送请求获取数据
+    数据繁杂 先打印出来 再解构
+22.加入购物车弹层            
+```
 ## Project setup
 ```
 npm install
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,53 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Login from '@/views/login/index.vue'
+import Layout from '@/views/layout/index.vue'
+import Search from '@/views/search/index.vue'
+import SearchList from '@/views/search/list.vue'
+import Prodetail from '@/views/prodetail/index.vue'
+import Pay from '@/views/pay/index.vue'
+import Myorder from '@/views/myorder/index.vue'
+import Home from '@/views/layout/home.vue'
+import Category from '@/views/layout/category.vue'
+import Cart from '@/views/layout/cart.vue'
+import User from '@/views/layout/user.vue'
+import store from '@/store'
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/login', component: Login },
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    component: Layout,
+    redirect: '/home',
+    children: [
+      { path: '/home', component: Home },
+      { path: '/category', component: Category },
+      { path: '/cart', component: Cart },
+      { path: '/user', component: User }
+    ]
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  { path: '/search', component: Search },
+  { path: '/searchlist', component: SearchList },
+  { path: '/prodetail/:id', component: Prodetail },
+  { path: '/pay', component: Pay },
+  { path: '/myorder', component: Myorder }
 ]
-
+const authUrl = ['/pay', '/myorder']
 const router = new VueRouter({
   routes
 })
-
+// 前置导航守卫
+router.beforeEach((to, from, next) => {
+  if (!authUrl.includes(to.path)) {
+    next()
+    return
+  }
+  const token = store.getters.token
+  if (token) {
+    next()
+  } else {
+    next('/login')
+  }
+})
 export default router
